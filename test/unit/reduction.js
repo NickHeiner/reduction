bde7677bbc573997cf18c92e14fbb026af692334
Nick Heiner <nick.heiner@opower.com>
2013-11-03 18:37:28 -0500

    fixing issue with how we call _.reduce

--- test/unit/reduction.js
+++ test/unit/reduction.js
@@ -8,11 +8,17 @@ describe('reduction', function () {
 
     it('handles a method name', function() {
         var args = [['foo'], ['bar', 'baz'], ['quux']];
-        expect(reduction(args, 'concat', [])).to.deep.equal(_.flatten(args));
+        expect(reduction(args, 'concat')).to.deep.equal(_.flatten(args));
     });
 
-    it('handles normal lodash usage');
+    it('handles normal lodash usage', function() {
+        expect(reduction([1, 2, 3], function(a, b) {
+            return a + b;
+        })).to.equal(6);
+    });
 
-    it('handles an operator');
+    it('handles an operator', function() {
+        expect(reduction([2, 4, 8], '*')).to.equal(2 * 4 * 8);
+    });
 
 });
