75a319e80fd150664976b0dd54b1eed6bbd20ac6
Nick Heiner <nick.heiner@opower.com>
2013-11-03 18:20:26 -0500

    adding a passing test

--- test/unit/reduction.js
+++ test/unit/reduction.js
@@ -1,7 +1,18 @@
 'use strict';
 
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    _ = require('lodash'),
+    reduction = require('../..');
 
-describe('', function () {
+describe('reduction', function () {
+
+    it('handles a method name', function() {
+        var args = [['foo'], ['bar', 'baz'], ['quux']];
+        expect(reduction(args, 'concat', [])).to.deep.equal(_.flatten(args));
+    });
+
+    it('handles normal lodash usage');
+
+    it('handles an operator');
 
 });
